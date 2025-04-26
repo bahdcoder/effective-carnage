d691aff14a43e9fc7776d7e091abbc129b8b9163
Frantz Kati <bahdcoder@gmail.com>
2025-04-26 17:56:29 +0100

    chore: fix linting issues

--- core/tests/unit/modules/users/users.service.spec.ts
+++ core/tests/unit/modules/users/users.service.spec.ts
@@ -4,41 +4,41 @@ import { resolve } from "@/utils/container/resolve"
 import { describe, test, beforeAll, afterAll, expect } from "vitest"
 
 describe("users.module > users.service", () => {
-  const ignitor = new IgnitorDev()
+	const ignitor = new IgnitorDev()
 
-  const { container } = ignitor.ctx()
+	const { container } = ignitor.ctx()
 
-  beforeAll(async () => ignitor.initialize())
-  afterAll(async () => ignitor.shutdown())
+	beforeAll(async () => ignitor.initialize())
+	afterAll(async () => ignitor.shutdown())
 
-  test("can compare and verify a user's password", async () => {
-    const usersService = resolve(container, "usersService")
-    const prisma = resolve(container, "prisma")
+	test("can compare and verify a user's password", async () => {
+		const usersService = resolve(container, "usersService")
+		const prisma = resolve(container, "prisma")
 
-    const mockUser = {
-      email: Date.now() + "test@example.com",
-      password: "password",
-    }
+		const mockUser = {
+			email: `${Date.now()}-test@example.com`,
+			password: "password",
+		}
 
-    const user = await prisma.user.create({
-      data: {
-        ...mockUser,
-        password: await bcrypt.hash(mockUser.password, 10),
-      },
-    })
+		const user = await prisma.user.create({
+			data: {
+				...mockUser,
+				password: await bcrypt.hash(mockUser.password, 10),
+			},
+		})
 
-    const isPasswordCorrect = await usersService.confirmPassword(
-      user,
-      mockUser.password
-    )
+		const isPasswordCorrect = await usersService.confirmPassword(
+			user,
+			mockUser.password,
+		)
 
-    expect(isPasswordCorrect).toBe(true)
+		expect(isPasswordCorrect).toBe(true)
 
-    const checkForWrongPassword = await usersService.confirmPassword(
-      user,
-      "WRONG_PASSWORD"
-    )
+		const checkForWrongPassword = await usersService.confirmPassword(
+			user,
+			"WRONG_PASSWORD",
+		)
 
-    expect(checkForWrongPassword).toBe(false)
-  })
+		expect(checkForWrongPassword).toBe(false)
+	})
 })
